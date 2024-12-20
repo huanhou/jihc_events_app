fa676874c985605a96da60c3e91e39aa2e16ee5a
Moldir <bolatmmoldir@gmail.com>
2024-12-20 12:16:19 +0500

    Updates

---
+++ precache-manifest.7d164dc29cf7ebdcbe4b319e355d8be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7290c31aefadbcb0bdeca67678915694",
+    "url": "/jihc_events_app/index.html"
+  },
+  {
+    "revision": "3af8b9615331f9832ac7",
+    "url": "/jihc_events_app/static/css/main.3bd981ef.chunk.css"
+  },
+  {
+    "revision": "41faeffc5429603d8031",
+    "url": "/jihc_events_app/static/js/2.2c9ab0fc.chunk.js"
+  },
+  {
+    "revision": "67ae3e0212c3a537d86a1f099983ebeb",
+    "url": "/jihc_events_app/static/js/2.2c9ab0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af8b9615331f9832ac7",
+    "url": "/jihc_events_app/static/js/main.020eab2a.chunk.js"
+  },
+  {
+    "revision": "c06cb658bd66d4bca634",
+    "url": "/jihc_events_app/static/js/runtime-main.24581942.js"
+  }
+]);
